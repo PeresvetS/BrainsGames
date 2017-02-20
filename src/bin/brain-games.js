7ac31e2b99e0a4c039ff9c13d2d32d22820e2d08
PeresvetS <niks280593@gmail.com>
2017-02-20 19:44:39 +0300

    I have added a few strings into brain-games.js, created index.js and imported new library.

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,9 @@
 #!/usr/bin/env node
+
+import readlineSync from '../';
+
+
 console.log("Welcome to the Brain Games!");
+
+const UserName = readlineSync.question('May I have your name? ');
+console.log('Hello ' + UserName + '!');
